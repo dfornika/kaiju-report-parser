8e24f5c146d4ccd07b3e9b7a9b2505971274a6bc
Dan Fornika <dfornika@gmail.com>
2017-12-06 11:05:35 -0800

    Initial commit

---
+++ kaiju_json_parser.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import sys
+import json
+
+f = open(sys.argv[1], 'r')
+max_columns = int(sys.argv[2])
+header = f.readline().rstrip('\n').split("\t")
+taxon = header[2]
+
+kaiju_data = []
+for line in f:
+    if not line.startswith("------"):
+        line = line.rstrip('\n').split("\t")
+        line_dict = {}
+        line_dict['percent_reads_in_clade'] = float(line[0].lstrip())
+        line_dict['number_reads_in_clade'] = int(line[1].lstrip())
+        line_dict['clade_name'] = line[2]
+        kaiju_data.append(line_dict)
+
+top_results = sorted(kaiju_data, key=lambda k: k['percent_reads_in_clade'], reverse=True)[:max_columns]
+print json.dumps(top_results)
